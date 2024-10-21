d7b46307bbe41d7d00e2c9a1f32997380feca3d6
DavidBlanco2825 <davidfernando2825@hotmail.com>
2024-10-21 17:23:06 -0500

    feat: update DynamoDBTemplateAdapter to use domain model and implement UserRepository
    - Modified DynamoDBTemplateAdapter to work with the domain model for improved consistency.
    - Implemented the UserRepository interface in DynamoDBTemplateAdapter to handle user-related persistence operations.

--- infrastructure/driven-adapters/dynamo-db/src/main/java/co/com/bancolombia/dynamodb/DynamoDBTemplateAdapter.java
+++ infrastructure/driven-adapters/dynamo-db/src/main/java/co/com/bancolombia/dynamodb/DynamoDBTemplateAdapter.java
@@ -1,8 +1,11 @@
 package co.com.bancolombia.dynamodb;
 
 import co.com.bancolombia.dynamodb.helper.TemplateAdapterOperations;
+import co.com.bancolombia.model.user.User;
+import co.com.bancolombia.model.user.gateways.UserRepository;
 import org.reactivecommons.utils.ObjectMapper;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedAsyncClient;
 import software.amazon.awssdk.enhanced.dynamodb.Key;
@@ -13,7 +16,7 @@ import java.util.List;
 
 
 @Repository
-public class DynamoDBTemplateAdapter extends TemplateAdapterOperations<Object /*domain model*/, String, UserEntity /*adapter model*/> /* implements Gateway from domain */ {
+public class DynamoDBTemplateAdapter extends TemplateAdapterOperations<User /*domain model*/, String, UserEntity /*adapter model*/> /* implements Gateway from domain */ implements UserRepository {
 
     public DynamoDBTemplateAdapter(DynamoDbEnhancedAsyncClient connectionFactory, ObjectMapper mapper) {
         /**
@@ -21,15 +24,33 @@ public class DynamoDBTemplateAdapter extends TemplateAdapterOperations<Object /*
          *  super(repository, mapper, d -> mapper.mapBuilder(d,ObjectModel.ObjectModelBuilder.class).build());
          *  Or using mapper.map with the class of the object model
          */
-        super(connectionFactory, mapper, d -> mapper.map(d, Object.class /*domain model*/), "table_name", "secondary_index" /*index is optional*/);
+        super(connectionFactory, mapper, d -> mapper.map(d, User.class /*domain model*/), "table_name", "secondary_index" /*index is optional*/);
     }
 
-    public Mono<List<Object /*domain model*/>> getEntityBySomeKeys(String partitionKey, String sortKey) {
+    @Override
+    public Flux<User> findAll() {
+        return this.scan()
+                .flatMapMany(Flux::fromIterable);
+    }
+
+    // maybe we can change the name so that it is not necessary to override the method
+    @Override
+    public Mono<User> findById(String email) {
+        return this.getById(email);
+    }
+
+    // check if it would be better to just define the delete(User user) in UserRepository
+    @Override
+    public Mono<Void> deleteById(String email) {
+        return this.getById(email).flatMap(user -> this.delete(user).then());
+    }
+
+    public Mono<List<User /*domain model*/>> getEntityBySomeKeys(String partitionKey, String sortKey) {
         QueryEnhancedRequest queryExpression = generateQueryExpression(partitionKey, sortKey);
         return query(queryExpression);
     }
 
-    public Mono<List<Object /*domain model*/>> getEntityBySomeKeysByIndex(String partitionKey, String sortKey) {
+    public Mono<List<User /*domain model*/>> getEntityBySomeKeysByIndex(String partitionKey, String sortKey) {
         QueryEnhancedRequest queryExpression = generateQueryExpression(partitionKey, sortKey);
         return queryByIndex(queryExpression, "secondary_index" /*index is optional if you define in constructor*/);
     }
