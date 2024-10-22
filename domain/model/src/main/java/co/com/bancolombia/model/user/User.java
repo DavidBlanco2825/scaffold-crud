f5763c2751b385900034f739d4566ca7a0bba1b8
DavidBlanco2825 <davidfernando2825@hotmail.com>
2024-10-22 18:16:47 -0500

    feat: add default constructor to User domain class using @NoArgsConstructor
    - Added a default constructor to the User domain class with the Lombok `@NoArgsConstructor` annotation.
    - This ensures compatibility with frameworks that require a no-argument constructor for object instantiation.

--- domain/model/src/main/java/co/com/bancolombia/model/user/User.java
+++ domain/model/src/main/java/co/com/bancolombia/model/user/User.java
@@ -1,15 +1,14 @@
 package co.com.bancolombia.model.user;
 
-//import lombok.NoArgsConstructor;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-//@NoArgsConstructor
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
 public class User {
