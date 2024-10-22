f5241ba473ef77d3cd3bd508eff9a3abc81ce20d
DavidBlanco2825 <davidfernando2825@hotmail.com>
2024-10-22 18:44:12 -0500

    feat: implement CRUD operations for users in Handler class
    - Added logic in the Handler class to handle CRUD operations for users.
    - Interfaced with UserUseCase to execute business logic for creating, reading, updating, and deleting users.

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/Handler.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/Handler.java
@@ -1,6 +1,10 @@
 package co.com.bancolombia.api;
 
+import co.com.bancolombia.model.user.User;
+import co.com.bancolombia.usecase.user.UserUseCase;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -9,21 +13,56 @@ import reactor.core.publisher.Mono;
 @Component
 @RequiredArgsConstructor
 public class Handler {
-//private  final UseCase useCase;
-//private  final UseCase2 useCase2;
 
-    public Mono<ServerResponse> listenGETUseCase(ServerRequest serverRequest) {
-        // usecase.logic();
-        return ServerResponse.ok().bodyValue("");
+    private static final String EMAIL = "email";
+    private final UserUseCase userUseCase;
+
+    public Mono<ServerResponse> saveUser(ServerRequest serverRequest) {
+        return serverRequest.bodyToMono(User.class)
+                .flatMap(userUseCase::addUser)
+                .flatMap(user -> ServerResponse.status(HttpStatus.CREATED)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .bodyValue(user)
+                );
     }
 
-    public Mono<ServerResponse> listenGETOtherUseCase(ServerRequest serverRequest) {
-        // useCase2.logic();
-        return ServerResponse.ok().bodyValue("");
+    public Mono<ServerResponse> findUserByEmail(ServerRequest serverRequest) {
+        String email = serverRequest.pathVariable(EMAIL);
+        return userUseCase.findUserByEmail(email)
+                .flatMap(user -> ServerResponse.status(HttpStatus.OK)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .bodyValue(user)
+                );
     }
 
-    public Mono<ServerResponse> listenPOSTUseCase(ServerRequest serverRequest) {
-        // usecase.logic();
-        return ServerResponse.ok().bodyValue("");
+    public Mono<ServerResponse> getAllUsers(ServerRequest serverRequest) {
+        return ServerResponse.status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(userUseCase.getAllUsers(), User.class);
+    }
+
+    public Mono<ServerResponse> setActiveByEmail(ServerRequest serverRequest) {
+        String email = serverRequest.pathVariable(EMAIL);
+        return userUseCase.setActiveByEmail(email)
+                .flatMap(user -> ServerResponse.status(HttpStatus.OK)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .bodyValue(user)
+                );
+    }
+
+    public Mono<ServerResponse> setInactiveByEmail(ServerRequest serverRequest) {
+        String email = serverRequest.pathVariable(EMAIL);
+        return userUseCase.setInactiveByEmail(email)
+                .flatMap(user -> ServerResponse.status(HttpStatus.OK)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .bodyValue(user)
+                );
+    }
+
+    public Mono<ServerResponse> deleteUser(ServerRequest serverRequest) {
+        String email = serverRequest.pathVariable(EMAIL);
+
+        return userUseCase.deleteUser(email)
+                .then(ServerResponse.noContent().build());
     }
 }
