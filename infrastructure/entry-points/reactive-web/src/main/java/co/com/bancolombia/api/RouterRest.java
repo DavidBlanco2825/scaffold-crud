50fbe5bba01c6d023b890a624ac014e7643941d2
DavidBlanco2825 <davidfernando2825@hotmail.com>
2024-10-22 18:47:17 -0500

    feat: define routing configuration for API endpoints using Spring WebFlux in RouterRest
    - Implemented API endpoint routing using Spring WebFlux's functional routing style in the RouterRest class.
    - Defined routes for handling incoming HTTP requests and mapped them to their corresponding handlers.

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/RouterRest.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/RouterRest.java
@@ -5,16 +5,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import static org.springframework.web.reactive.function.server.RequestPredicates.DELETE;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RequestPredicates.PUT;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
 public class RouterRest {
+
     @Bean
     public RouterFunction<ServerResponse> routerFunction(Handler handler) {
-        return route(GET("/api/usecase/path"), handler::listenGETUseCase)
-                .andRoute(POST("/api/usecase/otherpath"), handler::listenPOSTUseCase)
-                .and(route(GET("/api/otherusercase/path"), handler::listenGETOtherUseCase));
+        return route(POST("/users"), handler::saveUser)
+                .andRoute(GET("/users/{email}"), handler::findUserByEmail)
+                .and(route(GET("/users"), handler::getAllUsers))
+                .and(route(PUT("/users/active-user/{email}"), handler::setActiveByEmail))
+                .and(route(PUT("/users/inactive-user/{email}"), handler::setInactiveByEmail))
+                .and(route(DELETE("/users/delete-user/{email}"), handler::deleteUser));
     }
 }
