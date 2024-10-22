1bd6b9172974b89db7eb5dd1367c41108e84e491
DavidBlanco2825 <davidfernando2825@hotmail.com>
2024-10-22 18:29:07 -0500

    chore: remove unused methods and comments from DynamoDBTemplateAdapter
    - Cleaned up DynamoDBTemplateAdapter by removing unused methods and outdated comments.
    - Improved code readability and maintainability.

--- infrastructure/driven-adapters/dynamo-db/src/main/java/co/com/bancolombia/dynamodb/DynamoDBTemplateAdapter.java
+++ infrastructure/driven-adapters/dynamo-db/src/main/java/co/com/bancolombia/dynamodb/DynamoDBTemplateAdapter.java
@@ -8,15 +8,9 @@ import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedAsyncClient;
-import software.amazon.awssdk.enhanced.dynamodb.Key;
-import software.amazon.awssdk.enhanced.dynamodb.model.QueryConditional;
-import software.amazon.awssdk.enhanced.dynamodb.model.QueryEnhancedRequest;
-
-import java.util.List;
-
 
 @Repository
-public class DynamoDBTemplateAdapter extends TemplateAdapterOperations<User /*domain model*/, String, UserEntity /*adapter model*/> /* implements Gateway from domain */ implements UserRepository {
+public class DynamoDBTemplateAdapter extends TemplateAdapterOperations<User, String, UserEntity> implements UserRepository {
 
     public DynamoDBTemplateAdapter(DynamoDbEnhancedAsyncClient connectionFactory, ObjectMapper mapper) {
         /**
@@ -24,7 +18,7 @@ public class DynamoDBTemplateAdapter extends TemplateAdapterOperations<User /*do
          *  super(repository, mapper, d -> mapper.mapBuilder(d,ObjectModel.ObjectModelBuilder.class).build());
          *  Or using mapper.map with the class of the object model
          */
-        super(connectionFactory, mapper, d -> mapper.map(d, User.class /*domain model*/), "table_name", "secondary_index" /*index is optional*/);
+        super(connectionFactory, mapper, d -> mapper.map(d, User.class), "user_table");
     }
 
     @Override
@@ -43,22 +37,5 @@ public class DynamoDBTemplateAdapter extends TemplateAdapterOperations<User /*do
     @Override
     public Mono<Void> deleteById(String email) {
         return this.getById(email).flatMap(user -> this.delete(user).then());
-    }
-
-    public Mono<List<User /*domain model*/>> getEntityBySomeKeys(String partitionKey, String sortKey) {
-        QueryEnhancedRequest queryExpression = generateQueryExpression(partitionKey, sortKey);
-        return query(queryExpression);
-    }
-
-    public Mono<List<User /*domain model*/>> getEntityBySomeKeysByIndex(String partitionKey, String sortKey) {
-        QueryEnhancedRequest queryExpression = generateQueryExpression(partitionKey, sortKey);
-        return queryByIndex(queryExpression, "secondary_index" /*index is optional if you define in constructor*/);
-    }
-
-    private QueryEnhancedRequest generateQueryExpression(String partitionKey, String sortKey) {
-        return QueryEnhancedRequest.builder()
-                .queryConditional(QueryConditional.keyEqualTo(Key.builder().partitionValue(partitionKey).build()))
-                .queryConditional(QueryConditional.sortGreaterThanOrEqualTo(Key.builder().sortValue(sortKey).build()))
-                .build();
     }
 }
